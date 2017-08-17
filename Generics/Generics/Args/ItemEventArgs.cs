43a97b037c32c869f0b44393e528fce635339e3b
97100 <berkay.basoz@softtech.com.tr>
2017-08-17 19:35:36 +0300

    ItemEventArgSample eklendi.

---
+++ Generics/Generics/Args/ItemEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.Args
+{  
+    public delegate void ItemEventHandler<T>(object sender, ItemEventArgs<T> e);
+      
+    public class ItemEventArgs<T> : EventArgs
+    {  
+        public ItemEventArgs(T item)
+        {
+            Item = item;
+        } 
+        public T Item { get; protected set; } 
+
+        public static implicit operator ItemEventArgs<T>(T item) => new ItemEventArgs<T>(item);
+    }
+
+}
---
+++ Generics/ItemEventArgSample/Program.cs
@@ -0,0 +1,52 @@
+﻿using Generics.Args;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+
+namespace ItemEventArgSample
+{
+    public class Program
+    {
+        public class BaseForm 
+        {
+            public delegate void ItemEventHandler<T>(BaseForm sender, ItemEventArgs<T> e);
+            public event ItemEventHandler<Exception> OnException;
+  
+            protected virtual void HandleEx(ItemEventArgs<Exception> e)
+            {
+                OnException?.Invoke(this, e);
+            }
+
+            public void Run()
+            {
+                try
+                {
+                    RunThrowableStuff();
+                }
+                catch (Exception ex)
+                {
+                    HandleEx(ex);
+                }
+            }
+
+            private void RunThrowableStuff()
+            {
+                throw new ApplicationException("I don't want to work today");
+            }
+        }
+        static void Main(string[] args)
+        {
+            BaseForm frmSample = new BaseForm();
+            frmSample.OnException += FrmSample_OnException;
+            frmSample.Run();
+            Console.ReadLine();
+        }
+
+        private static void FrmSample_OnException(BaseForm sender, ItemEventArgs<Exception> e)
+        {
+            Console.WriteLine(e.Item);
+        }
+    }
+}
