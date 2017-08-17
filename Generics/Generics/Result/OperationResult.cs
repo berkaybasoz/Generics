16f72929c9b74d7a2318e9f1c4c432c431063cef
97100 <berkay.basoz@softtech.com.tr>
2017-08-17 22:57:18 +0300

    OperationResultSample eklendi

---
+++ Generics/Generics/Result/OperationResult.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.Result
+{
+    public class OperationResult<TResult>
+    {
+        private OperationResult()
+        {
+        }
+        public bool Success { get; private set; }
+        public TResult Result { get; private set; }
+        public string NonSuccessMessage { get; private set; }
+        public Exception Exception { get; private set; }
+        public static OperationResult<TResult> CreateSuccessResult(TResult result)
+        {
+            return new OperationResult<TResult> { Success = true, Result = result };
+        }
+        public static OperationResult<TResult> CreateFailure(string nonSuccessMessage)
+        {
+            return new OperationResult<TResult> { Success = false, NonSuccessMessage = nonSuccessMessage };
+        }
+        public static OperationResult<TResult> CreateFailure(Exception ex)
+        {
+            return new OperationResult<TResult>
+            {
+                Success = false,
+                NonSuccessMessage = String.Format("{0}{1}{1}{2}", ex.Message, Environment.NewLine, ex.StackTrace),
+                Exception = ex
+            };
+        }
+    }
+}
---
+++ Generics/OperationResultSample/Program.cs
@@ -0,0 +1,47 @@
+﻿using Generics.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationResultSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperationResult<Student> studentResult = DoSomeStuff(12);
+            if (studentResult.Success)
+            {
+                Console.WriteLine("Student {0} name {1}", 
+                    studentResult.Result.Id, studentResult.Result.Name);
+            }
+            else
+            {
+                Console.WriteLine(studentResult.Exception);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static OperationResult<Student> DoSomeStuff(int studentId)
+        {
+            if (studentId > 0)
+            {
+                Student student = QueryDatabase<Student>(studentId);
+                return OperationResult<Student>.CreateSuccessResult(student);
+            }
+            else
+            {
+                return OperationResult<Student>
+                    .CreateFailure(new ApplicationException(nameof(studentId) + " must be bigger than zero"));
+            }
+        }
+
+        public static Student QueryDatabase<T>(int studentId)
+        {
+            return new Student() { Id = 1056, Name = "Berkay Başöz" };
+        }
+    }
+}
